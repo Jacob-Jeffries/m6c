4c595424c096f9badd11127c80f8ee2b05aa933f
Jacob Jeffries <jeffrij85@gmail.com>
2023-01-08 21:10:24 -0500

    playing with test data

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,8 +5,22 @@ function test(){
   })
   .then(function(data) {
     console.log(data);
+    displayWeather(data);
   })
 };
 
 test();
+// console.log(weather);
 
+function displayWeather(weather){
+  console.log(weather);
+  console.log(weather.list[0].main.temp);
+  console.log(weather.list[0].main.feels_like);
+  console.log(weather.list[0].main.temp_min);
+  console.log(weather.list[0].main.temp_max);
+  console.log(weather.list[0].main.humidity);
+  console.log(weather.list[0].weather[0].id);
+  console.log(weather.list[0].weather[0].main);
+  console.log(weather.list[0].weather[0].description);
+  console.log(weather.list[0].clouds.all);
+};
