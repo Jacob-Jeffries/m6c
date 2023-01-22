980fd3f5cdb996408d7458580e18058caf554313
Jacob Jeffries <jeffrij85@gmail.com>
2023-01-22 09:36:58 -0500

    python script now takesin the JSON (list), extracts all the US cities and sorts them A-Z. Next I need to separate them into state buckts and resort. I will try the split search method in JS later.

--- data/sortCities.py
+++ data/sortCities.py
@@ -1,12 +1,35 @@
 import json
 
+us_cities = []
+
 #Golly I forgot I was using python and the pwd is different!
 with open("/mnt/d/bootcamp/lessons/m6c/data/cityList.json") as data:
   cityList = json.load(data)
 
 print(type(cityList))
-print(cityList[0]["country"])
-print(cityList[1]["country"])
-print(cityList[2]["country"])
+print(cityList[0])
+print(cityList[0]["coord"])
+print(cityList[0]["coord"]["lon"])
+print(cityList[0]["coord"]["lat"])
 print(cityList[3]["country"])
 print(cityList[4]["country"])
+
+for x in cityList:
+  if x["country"] == "US":
+    us_cities.append(x)
+
+print(us_cities[0]["name"])
+print(us_cities[1]["name"])
+print(us_cities[2]["name"])
+print(us_cities[3]["name"])
+print(us_cities[4]["name"])
+
+sorted_us_cities = sorted(us_cities, key=lambda X: X["name"])
+
+
+print(sorted_us_cities[0]["name"], sorted_us_cities[0]["state"])
+print(sorted_us_cities[1]["name"])
+print(sorted_us_cities[2]["name"])
+print(sorted_us_cities[3]["name"])
+print(sorted_us_cities[4]["name"])
+
