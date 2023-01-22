0a8b2cbf17955ee80b3cf30d57993778bd02170d
Jacob Jeffries <jeffrij85@gmail.com>
2023-01-22 08:45:15 -0500

    Python script is taking the city list json and converting it into a list - instead of a dictionary??

--- data/sortCities.py
+++ data/sortCities.py
@@ -1,8 +1,12 @@
 import json
 
-with open("./data/cityList.json") as data:
+#Golly I forgot I was using python and the pwd is different!
+with open("/mnt/d/bootcamp/lessons/m6c/data/cityList.json") as data:
   cityList = json.load(data)
 
-print(cityList.headline())
-print(cityList.headline())
-print(cityList.headline())
+print(type(cityList))
+print(cityList[0]["country"])
+print(cityList[1]["country"])
+print(cityList[2]["country"])
+print(cityList[3]["country"])
+print(cityList[4]["country"])
