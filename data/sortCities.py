c3baae528517a467487134dbaa7e540934c1f635
Jacob Jeffries <jeffrij85@gmail.com>
2023-01-22 08:28:20 -0500

    modified

--- data/sortCities.py
+++ data/sortCities.py
@@ -1,8 +1,7 @@
 import json
 
-with open("./data/cityList.json") as data
-
-cityList = json.load(data)
+with open("./data/cityList.json") as data:
+  cityList = json.load(data)
 
 print(cityList.headline())
 print(cityList.headline())
